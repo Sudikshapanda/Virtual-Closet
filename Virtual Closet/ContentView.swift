d0489e8136f112d76909fb011cda98fc447c0b5b
Sudikshapanda <Sudikshapanda9@gmail.com>
2023-05-17 21:37:16 -0400

    vd1

--- Virtual Closet/ContentView.swift
+++ Virtual Closet/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Barbie!")
+            Text("Drip Master")
         }
         .padding()
     }

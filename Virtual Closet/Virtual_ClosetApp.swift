405383891f110a285bd255472115144161afa92b
Sudikshapanda <Sudikshapanda9@gmail.com>
2023-05-17 20:56:49 -0400

    Initial Commit

---
+++ Virtual Closet/Virtual_ClosetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Virtual_ClosetApp.swift
+//  Virtual Closet
+//
+//  Created by Sudiksha Panda on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Virtual_ClosetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

72f2ce93d792b35e622e576cf2acdfe9823a024b
Sudikshapanda <Sudikshapanda9@gmail.com>
2023-05-19 14:19:18 -0400

    home screen buttons

--- Virtual Closet/ContentView.swift
+++ Virtual Closet/ContentView.swift
@@ -8,17 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State public var showingCloset = false
+    @State private var showingCloset = false
+    @State private var showingAddItem = false
+    @State private var showingCleanCloset = false
     var body: some View {
         if showingCloset {
             MyClosetView()
         }else {
-            AppHome()
+            //AppHome()
+            VStack {
+                Image("my closet")
+                    .resizable()
+                    .frame(width:1056, height:1024)
+                    .offset(y: -110)
+                    .animation(.easeInOut, value: true)
+                HStack {
+                    Button(action: {showingCloset.toggle()}){
+                        Text("My Closet")
+                    }
+                    .offset(y: -568)
+                    //Button(action: {sh})
+                }
+            }
+            .padding()
         }
-        
-        
-    }
 }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
